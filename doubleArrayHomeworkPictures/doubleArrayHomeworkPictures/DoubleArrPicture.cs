5b95b4b6aafa71233c54833e85cec062eaf2f3c4
Asus <vladimirrogozin007@gmail.com>
2021-05-28 21:29:52 +0300

    init

---
+++ doubleArrayHomeworkPictures/doubleArrayHomeworkPictures/DoubleArrPicture.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace picture_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание:
+            //1.3:
+            //ххххх
+            //0ххх0
+            //00х00
+            //0ххх0
+            //ххххx
+            //P.S : не получилось
+            int[,] doubleArray = new int[5, 5];
+            char[,] doubleArrayX = new char[5, 5];
+            for (int i = 0; i < doubleArrayX.GetLength(0); i++)
+            {
+                for (int j = 0; j < doubleArrayX.GetLength(1); j++)
+                {
+                    if (j>=0 || i>=0 )
+                    {
+                        doubleArrayX[i, j] = 'X';
+                    }
+                    else 
+                    {
+                        doubleArrayX[i, j] = '0';
+                    }
+                    Console.Write($"{doubleArrayX[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ doubleArrayHomeworkPictures/doubleArrayHomeworkPictures/DoubleArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace picture_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание:
+            //1.2:
+            //00x00
+            //00х00
+            //xxххx
+            //00x00
+            //00x00
+            int[,] doubleArray = new int[5, 5];
+            char[,] doubleArrayX = new char[5, 5];
+            for (int i = 0; i < doubleArrayX.GetLength(0); i++)
+            {
+                for (int j = 0; j < doubleArrayX.GetLength(1); j++)
+                {
+                    if (i == 2 || j == 2)
+                    {
+                        doubleArrayX[i,j] = 'X';
+                    }
+                    else
+                    {
+                        doubleArrayX[i, j] = '0';
+                    }
+                    Console.Write($"{doubleArrayX[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
